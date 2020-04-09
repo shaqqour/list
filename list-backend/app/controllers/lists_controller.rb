156f7ab9ae451a468d78c939363623715e6eb62c
Shaqqour <mshaqqourm@gmail.com>
2020-04-09 14:49:30 -0700

    Render json from listsController

--- list-backend/app/controllers/lists_controller.rb
+++ list-backend/app/controllers/lists_controller.rb
@@ -1,2 +1,11 @@
 class ListsController < ApplicationController
+
+    def index
+        lists = List.all
+        render json: lists.to_json(
+            :include => { :items => { only: [:id, :name, :status, :priority, :due_date] } },
+            :only => [:id, :name]
+        )
+    end
+
 end
