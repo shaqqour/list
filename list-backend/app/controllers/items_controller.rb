b41e3fd59fb0305c4d3ab11a8671adf5e5cc1039
Shaqqour <mshaqqourm@gmail.com>
2020-04-11 16:55:52 -0700

    Create an item

--- list-backend/app/controllers/items_controller.rb
+++ list-backend/app/controllers/items_controller.rb
@@ -1,2 +1,15 @@
 class ItemsController < ApplicationController
+
+    def create
+        list = List.find_by(id: params[:list_id])
+        item = list.items.create(item_params)
+        
+        render json: item.to_json
+    end
+    
+    private
+    def item_params
+        params.require(:item).permit(:name, :status, :priority, :due_date, :list_id)
+    end
+
 end
