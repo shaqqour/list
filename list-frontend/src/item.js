c3c6f1a06c45ccc13125ebc433ade58ede22b643
Shaqqour <mshaqqourm@gmail.com>
2020-04-15 16:01:34 -0700

    Add addToDom function to item class

--- list-frontend/src/item.js
+++ list-frontend/src/item.js
@@ -8,6 +8,35 @@ class Item {
         this.list_id = attributes.list_id;
     }
 
+    addToDOM() {
+        let item = this;
+        //find the div and ul where the item will be added
+        const div = document.getElementById(this.list_id)
+        const ul = div.lastChild;
+        const li = document.createElement("li");
+
+        //create the doing button to move it to the doing list
+        const doingButton = document.createElement("button");
+        doingButton.className = "doing";
+        doingButton.innerHTML = "Doing";
+
+        //create the delete button in case the user want to delete the item
+        const deleteButton = document.createElement("button");
+        deleteButton.className = "delete";
+        deleteButton.type = "submit";
+        deleteButton.innerHTML = "Delete";
+        deleteButton.addEventListener("click", function (e) {
+            item.delete();
+            e.target.parentElement.remove();
+        });
+
+        li.innerHTML = item.name;
+        li.appendChild(deleteButton);
+        li.appendChild(doingButton);
+        ul.appendChild(li);
+        div.appendChild(ul);
+    }
+
     delete() {
         let configObj = { method: "DELETE" };
         fetch(ITEMS_URL + "/" + this.id, configObj);
--- list-frontend/src/list.js
+++ list-frontend/src/list.js
@@ -115,32 +115,7 @@ class List {
     }
 
     addItem(item) {
-
-        //find the div and ul where the item will be added
-        const div = document.getElementById(item.list_id)
-        const ul = div.lastChild;
-        const li = document.createElement("li");
-
-        //create the doing button to move it to the doing list
-        const doingButton = document.createElement("button");
-        doingButton.className = "doing";
-        doingButton.innerHTML = "Doing";
-
-        //create the delete button in case the user want to delete the item
-        const deleteButton = document.createElement("button");
-        deleteButton.className = "delete";
-        deleteButton.type = "submit";
-        deleteButton.innerHTML = "Delete";
-        deleteButton.addEventListener("click", function (e) {
-            item.delete();
-            e.target.parentElement.remove();
-        });
-
-        li.innerHTML = item.name;
-        li.appendChild(deleteButton);
-        li.appendChild(doingButton);
-        ul.appendChild(li);
-        div.appendChild(ul);
+        item.addToDOM();
     }
 
     addDeleteListButton() {
